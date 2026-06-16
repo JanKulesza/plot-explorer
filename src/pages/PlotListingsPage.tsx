2b52179182dfcfe3a933607e54164dd23c53736e
JanKulesza <jan.kulesza87@gmail.com>
2026-06-16 21:02:52 +0200

    Install shadcn

--- src/pages/PlotListingsPage.tsx
+++ src/pages/PlotListingsPage.tsx
@@ -1,6 +1,8 @@
+import { Button } from "../components/ui/button"
+
 const PlotListingsPage = () => {
   return (
-    <div>PlotListingsPage</div>
+    <Button variant="outline">Plot Listings Page</Button>
   )
 }
 
